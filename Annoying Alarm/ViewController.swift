aeb369506459d8c9fd74babfbc8af9334b12a86b
Andres Sanchez <Sanchezandres5746@gmail.com>
2022-11-29 09:50:50 -0800

    Returned my changes Spent Time trying to do a UIDatePicker but realized I already had one

--- Annoying Alarm/ViewController.swift
+++ Annoying Alarm/ViewController.swift
@@ -13,39 +13,12 @@ class ViewController: UIViewController {
     //THis stuff loads onto the screen
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-     
-        //Button frame,location,text, variable to do if tapped (old button)
-   /*     title = "Welcome To Annoying Alarm!"
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
-        view.addSubview(button)
-        button.center = view.center
-        button.backgroundColor = .systemOrange
-        button.setTitle("Next Screen", for: .normal)
-        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
-        configureItem()
+    }
+
+    @IBAction func timeSet(_ sender: Any, forEvent event: UIEvent) {
         
     }
-    //OLD BUTTON JUNK Just in case if decide to go back
-    private func  configureItem(){
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
-            barButtonSystemItem: .add,
-            target: self,
-            action: nil
-        )
-        
-    }
-    //if bytton is tapped do
-    @objc func didTapButton() {
-       let vc = UIViewController()
-        // title for second screen
-        vc.title = "Select Time"
-        vc.view.backgroundColor = .lightGray
-        
-      //  vc.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "???", style: .done, target: self, action: nil) //(Possible button for later)
-        navigationController?.pushViewController(vc, animated: true)
-    */
-    }
+    
 }
  
 
